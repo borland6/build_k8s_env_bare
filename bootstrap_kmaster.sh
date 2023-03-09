864faada932ba7fca9db414f5a155d96c041600f
borland6 <borland6@gmail.com>
2023-03-09 21:10:06 +0800

    Update bootstrap_kmaster.sh
    modify pod-network-cidr to 10.244.0.0/16
    modify calico.yaml --> set cidr to 10.244.0.0/16

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -4,7 +4,7 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 echo "[TASK 3] Deploy Calico network"
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
@@ -13,12 +13,11 @@ echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
 
 echo "[TASK 5] copy kubeconfig file for root user"
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo mkdir -p /root/.kube
+sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
+sudo chown root:root /root/.kube/config
 
 echo "[TASK 6] copy kubeconfig file for vagrant user"
-su - vagrant
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf $/home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
